f7ebfb5b0f2f533cfc3caeda25b458fea90832b6
Krishna <ridpatel2907@gmail.com>
2020-11-07 21:05:06 +0700

    update our menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import {Switch,Route,Link} from  'react-router-dom';
+
+function Hatspage(props){
+  console.log(props)
+      return(
+        <div>
+         <h1>HatsPage</h1>
+
+         <Link to='/'>Homepage</Link>
+         <button onClick={()=>props.history.push('/')}> go to homepage</button>
+        </div>
+      )
+}
+
 
 function App() {
   return (
     <div>
-      <Homepage/>
+    <Switch>
+      <Route exact path='/' component={Homepage}/>
+      <Route  path='/hats' component={Hatspage}/>
+      
+    </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import {withRouter} from 'react-router-dom';
 
-const MenuItem=({title,imageUrl,id,size})=>(
-            <div className={`${size} menu-item`}>
-            <div className='background-image' style={{
+const MenuItem=({title,imageUrl,id,size,linkUrl,history,match})=>(
+           <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
+
+                <div className='background-image' style={{
                          backgroundImage:`url(${imageUrl})`
                        }}/>
+
                 <div className="content">
                     <h1 className="title">{title.toUpperCase()}</h1>
                     <span className="subtitle">SHOP NOW</span>
@@ -13,4 +16,7 @@ const MenuItem=({title,imageUrl,id,size})=>(
             </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
+//withrouter() is a higher order function that takes component as a parameter
+//and returns modified component
+//with use of withRouter() we can have access to history,match,and location property of props
