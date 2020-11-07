687e06bdfb6a13fdf95528d07ede5dd89de98285
Krishna <ridpatel2907@gmail.com>
2020-11-08 01:03:50 +0700

    updating our app with shop page,collection-preview component and collection-item component added

--- src/App.js
+++ src/App.js
@@ -2,26 +2,14 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import {Switch,Route,Link} from  'react-router-dom';
-
-function Hatspage(props){
-  console.log(props)
-      return(
-        <div>
-         <h1>HatsPage</h1>
-
-         <Link to='/'>Homepage</Link>
-         <button onClick={()=>props.history.push('/')}> go to homepage</button>
-        </div>
-      )
-}
-
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
     <Switch>
       <Route exact path='/' component={Homepage}/>
-      <Route  path='/hats' component={Hatspage}/>
+      <Route  path='/shop' component={ShopPage}/>
       
     </Switch>
     </div>
