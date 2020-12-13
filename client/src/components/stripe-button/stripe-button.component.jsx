5922556b862018e62d326765d18db94a3c66e5c2
Krishna <ridpatel2907@gmail.com>
2020-12-13 14:20:02 +0700

    separating client and server backend

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,7 +1,7 @@
 //create a new account for stripe
 /*here in developer section you will get publishable API KEY ,and secret key as well,
-we will copy  this this publishable API key in our code,these code will tell stripe that 
-our app  want to access this stripe account.for now we will not use secret key(its a live key),
+we will copy  this publishable API key in our code,these code will tell stripe that 
+our app  want to access this stripe account.for now we will not use secret key(its a live key,reside on server),
 bcoz it will be used at server side,for applying charges*/
 /*yarn add react-stripe-checkout*/
 //above libarary will provide us react bound version of stripe checkout button
@@ -9,14 +9,36 @@ bcoz it will be used at server side,for applying charges*/
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
-const onToken=(token)=>{
-    console.log(token);
-    alert("payment successfull");
-}
+//payment request  with axios
+import axios from 'axios';
+
+
 const StripeCheckoutButton=({price})=>{
 //stipe works with cents so we need to multiply original amount with 100
-const priceForStripe={price}*100;
+const priceForStripe=price*100;
 const publishableKey="pk_test_51HqX6LEMcYHTtZjO2HO3B8a3youFbbccVGd0RqIdDoH2jnKbDYpjeMzGqraPj5AQbG3M5EqDobBHx0NagmkQDb0l005M9SzI6I";
+
+const onToken=(token)=>{
+    console.log(token);
+//send request to /payment route to the server(you can find /payment route in server.js file.)
+    axios({
+        url:'payment',
+        method:'post',
+        data:{
+            description:'software development services',
+            amount:priceForStripe,
+            token:token
+        }
+        
+    }).then(response=>{
+        alert("Payment successfull");
+    }).catch(error=>{
+        console.log('Payment error');
+        console.log(error);
+        alert('There was an issue with your payment.Please make sure that you have provided used credit card');    
+                })
+
+}
 
 return(
     <StripeCheckout
--- client/src/pages/checkout/checkoutpage.component.jsx
+++ client/src/pages/checkout/checkoutpage.component.jsx
@@ -36,7 +36,9 @@ const CheckoutPage=({cartItems,total})=>{
             }
 
             <div className="total">Total : {total}$</div>
+
             <StripeCheckoutButton price={total}/>
+            
             <div className='test-warning'>
                 *Please use the following test credit card for payments*
                 <br/>
